fb575b61f217fe3a5b89207cc67b4da9531a6b1f
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-01-17 20:42:18 +0500

    привет домино

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ class GameField:
         self.free_tiles = free_tile
         self.finish_tile = finish_tile
 
-    def render(self, screen):
+    def render(self, screen, koeff):
         for y in range(self.height):
             for x in range(self.width):
                 for i in range(3):
                     image = self.map.get_tile_image(x, y, i)
                     if image is not None:
-                        screen.blit(image, (x * self.tile_size, y * self.tile_size))
+                        screen.blit(image, (x * self.tile_size + koeff[0],
+                                            y * self.tile_size + koeff[1]))
 
     def get_tile_id(self, position):
         if self.map.get_tile_gid(*position, 1) in self.map.tiledgidmap.keys():
@@ -41,8 +42,9 @@ class Hero:
     def set_position(self, position):
         self.x, self.y = position
 
-    def render(self, screen):
-        center = self.x * TILE_SIZE + TILE_SIZE // 2, self.y * TILE_SIZE + TILE_SIZE // 2
+    def render(self, screen, koeff):
+        center = self.x * TILE_SIZE + TILE_SIZE // 2 + koeff[0], \
+                 self.y * TILE_SIZE + TILE_SIZE // 2 + koeff[1]
         pygame.draw.circle(screen, (255, 255, 255), center, radius=TILE_SIZE // 2)
 
 
@@ -52,9 +54,9 @@ class Game:
         self.lab = field
         self.hero = hero
 
-    def render(self, screen):
-        self.lab.render(screen)
-        self.hero.render(screen)
+    def render(self, screen, koeff):
+        self.lab.render(screen, koeff)
+        self.hero.render(screen, koeff)
 
     def update_hero(self):
         next_x, next_y = self.hero.get_position()
@@ -66,9 +68,21 @@ class Game:
             next_y -= 1
         if pygame.key.get_pressed()[pygame.K_DOWN]:
             next_y += 1
-        print(self.lab.get_tile_id((next_x, next_y)))
+
         if self.lab.is_free((next_x, next_y)):
             self.hero.set_position((next_x, next_y))
+
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx += -(target.x * TILE_SIZE + self.dx + TILE_SIZE - WINDOW_WIDTH // 2)
+        self.dy += -(target.y * TILE_SIZE + self.dy + TILE_SIZE - WINDOW_HEIGHT // 2)
 
 
 def main():
@@ -80,6 +94,7 @@ def main():
     field = GameField("map.tmx", [78, 79, 80, 48, 49, 50, 108, 109, 110], 0)
     hero = Hero((3, 13))
     game = Game(field, hero)
+    camera = Camera()
 
     running = True
     clock = pygame.time.Clock()
@@ -90,7 +105,8 @@ def main():
         screen.fill((255, 228, 225))
 
         game.update_hero()
-        game.render(screen)
+        game.render(screen, (camera.dx, camera.dy))
+        camera.update(hero)
 
         clock.tick(FPS)
         pygame.display.flip()
