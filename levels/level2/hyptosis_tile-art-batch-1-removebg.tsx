f7c129010d75dea9eed5989f5c04824705f06c79
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-01-23 20:27:22 +0500

    музыка

--- levels/level2/hyptosis_tile-art-batch-1-removebg.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.12.18" name="hyptosis_tile-art-batch-1-removebg" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="hyptosis_tile-art-batch-1-removebg.png" width="960" height="960"/>
-</tileset>
